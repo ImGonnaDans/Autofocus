e3580178310cce715fa4f7ad589355ee98893932
ImGonnaDans <pyb_personal@163.com>
2020-09-09 17:28:13 +0800

    add gradient function

--- Autofocus.py
+++ Autofocus.py
@@ -1,3 +1,120 @@
+
+// 方差锐度
+double Squared_Grad(Mat img)
+{
+	double Squared = 0;;
+	for (int i = 0; i < img.rows - 1; i++)
+	{
+		unsigned short* Prow = img.ptr<unsigned  short>(i);
+		unsigned short* Prow_next = img.ptr<unsigned  short>(i + 1);
+		for (int j = 0; j < img.cols; j++)
+		{
+			Squared += pow((double)abs(Prow[j] - Prow_next[j]), 2) + pow((double)abs(Prow[j] - Prow[j + 1]), 2);
+		}
+	}
+	return Squared;
+}
+
+// TenenGrad
+double Tenen_grad(Mat img)
+{
+	Mat imgSobel, imgSobel_x, imgSobel_y;
+    Rect ROI = Rect(106, 136, 360, 300);
+    img = img(ROI);
+    //imshow("asd", ImageToBuffer(ROI));
+    //waitKey(10);
+	//medianBlur(img, img, 5);
+    filter2D(img, img,CV_32F, Mat::ones(5,5,CV_16UC1));
+    Sobel(img, imgSobel_x, CV_16UC1, 1, 0);
+    Sobel(img, imgSobel_y, CV_16UC1, 0, 1);
+    imgSobel = imgSobel_x + imgSobel_y;
+    imgSobel.convertTo(imgSobel, CV_32FC1);
+    pow(imgSobel, double(2), imgSobel);
+	//图像的平均灰度
+	double meanValue = 0.0;
+	meanValue = sum(imgSobel)[0];
+    //cout << meanValue << endl;
+	return meanValue;
+}
+
+// BrenerGrad
+double Brenner_grad(Mat img)
+{
+    Rect ROI = Rect(216, 176, 200, 160);
+    img = img(ROI);
+    medianBlur(img, img, 5);
+	double Squared = 0;;
+	for (int i = 0; i < img.rows - 2; i++)
+	{
+		unsigned short* Prow = img.ptr<unsigned  short>(i);
+		for (int j = 0; j < img.cols; j++)
+		{
+			Squared += pow((double)(Prow[j + 2] - Prow[j]), 2);
+		}
+	}
+	return Squared;
+}
+
+// 拉普拉斯锐度
+float Laplacin_grad(Mat img)
+{
+	Mat imageSobel;
+	Mat Filtered;
+	medianBlur(img, Filtered, 3);
+	Laplacian(Filtered, imageSobel, CV_16UC1, 1, 1);
+
+	//图像的平均灰度
+	double meanValue = 0.0;
+	meanValue = mean(imageSobel)[0];
+
+    SaveImagePro();
+
+	return meanValue;
+}
+
+// 差分锐度
+float Variance_function(Mat img)
+{
+	float meanValue = mean(img)[0];
+	double Variance = 0;;
+	for (int i = 0; i < img.rows; i++)
+	{
+		unsigned short* Prow = img.ptr<unsigned  short>(i);
+		for (int j = 0; j < img.cols; j++)
+		{
+			Variance += pow((double)abs(Prow[j] - meanValue), 2);
+		}
+	}
+	return Variance;
+}
+
+// 变频锐度
+float freShifting_grad(Mat img, int freq)
+{
+    Rect selectRect = Rect(216, 176, 200, 160);
+    Mat selectImg = img(selectRect);
+	Mat imgC, imgD, imgR;
+	float freShifting_grad = 0;
+	for (int i = 0; i < selectImg.cols - freq * 2; i++)
+	{
+		for (int j = 0; j < selectImg.rows - freq * 2; j++)
+		{
+			Rect ROI = Rect(i, j, freq, freq);
+			//imgC = rectangle(img, Point(i, j), Point(i + freq, j + freq), Scalar(0),1,0,0);
+			imgC = selectImg(ROI);
+			ROI = Rect(i + freq, j, freq, freq);
+			imgR = selectImg(ROI);
+			ROI = Rect(i, j + freq, freq, freq);
+			imgD = selectImg(ROI);
+			freShifting_grad += abs(mean(imgC)[0] - mean(imgD)[0]) + abs(mean(imgC)[0] - mean(imgR)[0]);
+            
+		}
+	}
+    //SaveImagePro();
+    return freShifting_grad;
+}
+
+
 // 动量自动对焦
 void autofocus_momentum()
 {
